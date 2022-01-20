c04398874a7beaddcd57c9849001d325f3200eb6
nazim725 <nazimhabib77@gmail.com>
2022-01-20 10:01:35 -0800

    added

--- backend/server.js
+++ backend/server.js
@@ -30,14 +30,11 @@ app.use("/api/message", messageRoutes);
 const __dirname1 = path.resolve();
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname1, "../frontend/build")));
+  app.use(express.static(path.join(__dirname1, "/frontend/build")));
 
   app.get("*", (req, res) =>
-    res.sendFile(path.join(__dirname1, "../frontend", "build", "index.html"))
+    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
   );
-  // app.get("*", function (req, res) {
-  //   res.sendFile("index.html");
-  // });
 } else {
   app.get("/", (req, res) => {
     res.send("API is running..");
